9cb333e2a205b0074e48a4a5275576271a72772d
Mustak Ahmmed <rmmostak@gmail.com>
2023-05-08 23:12:20 +0600

    Update constants.dart

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
 
-const baseURL = 'https://server.smartaquaculture.info/api';
-const loginURL = '$baseURL/login';
-const regURL = '$baseURL/register';
-const logoutURL = '$baseURL/logout';
-const userURL = '$baseURL/user';
-const editUserURL = '$baseURL/save';
-const sendURL = '$baseURL/submit';
-const historyURL = '$baseURL/history';
-
 const serverError = 'তথ্য ভান্ডারে কিছু সমস্যা হয়েছে';//'Server Error';
 const unAuthError = 'আপনার প্রবেশের অনুমোদন নেয়';//'Unauthorized';
 const somethingWrong = 'কিছু সমস্যা হয়েছে, অনুগ্রহ করে আবার চেষ্টা করুন';//'Something went wrong, Please try again later.';
